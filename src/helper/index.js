55c88d30283f5e1393c8ad730b871f6f6bfc1d16
Edgar Rafael Jr. <etrafael.dev@gmail.com>
2026-04-06 18:21:08 +0800

    refactor: rename to checkTargetedConsoleLog

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,4 +1,4 @@
 export { extractLogs } from "./extractLogs.js";
-export { isTargetedConsoleLog } from "./isTargetedConsoleLog.js";
+export { checkTargetedConsoleLog } from "./checkTargetedConsoleLog.js";
 export { rescueLeadingComments } from "./rescueLeadingComments.js";
 export { commentOutLogs } from "./commentOutLogs.js";
